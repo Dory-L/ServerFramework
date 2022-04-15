7f938d31720cb1800660352c6c1700ac21c5168b
Dory-L <liu_lanyi@126.com>
2022-04-15 22:31:43 +0800

    完善配置系统

--- dory/config.h
+++ dory/config.h
@@ -5,6 +5,12 @@
 #include <boost/lexical_cast.hpp> //内纯转化
 #include "log.h"
 #include <yaml-cpp/yaml.h>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
 
 namespace dory
 {
@@ -14,7 +20,7 @@ public:
     ConfigVarbase(const std::string& name, const std::string& description = "")
         :m_name(name)
         ,m_description(description) {
-        std::transform(m_name.begin(), m_name.end(), m_name.begin(), ::tolower);
+        std::transform(m_name.begin(), m_name.end(), m_name.begin(), ::tolower);//字母都转为小写
     }
     virtual ~ConfigVarbase() {}
     const std::string& getName() const { return m_name; }
@@ -36,6 +42,7 @@ public:
     }
 };
 
+//以下为偏特化类型
 template<class T>
 class LexicalCast<std::string, std::vector<T> > {
 public:
@@ -59,6 +66,158 @@ public:
         YAML::Node node;
         for (auto& i : v) {
             node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+        }
+        std::stringstream ss;
+        ss << node;
+        return ss.str();
+    }
+};
+
+template<class T>
+class LexicalCast<std::string, std::list<T> > {
+public:
+    std::list<T> operator() (const std::string& v) {
+        YAML::Node node = YAML::Load(v);
+        typename std::list<T> lst;
+        std::stringstream ss;
+        for (size_t i = 0; i < node.size(); ++i) {
+            ss.str("");
+            ss << node[i];
+            lst.push_back(LexicalCast<std::string, T>()(ss.str()));
+        }
+        return lst;
+    }
+};
+
+template<class T>
+class LexicalCast<std::list<T>, std::string> {
+public:
+    std::string operator() (const std::list<T>& v) {
+        YAML::Node node;
+        for (auto& i : v) {
+            node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+        }
+        std::stringstream ss;
+        ss << node;
+        return ss.str();
+    }
+};
+
+template<class T>
+class LexicalCast<std::string, std::set<T> > {
+public:
+    std::set<T> operator() (const std::string& v) {
+        YAML::Node node = YAML::Load(v);
+        typename std::set<T> st;
+        std::stringstream ss;
+        for (size_t i = 0; i < node.size(); ++i) {
+            ss.str("");
+            ss << node[i];
+            st.insert(LexicalCast<std::string, T>()(ss.str()));
+        }
+        return st;
+    }
+};
+
+template<class T>
+class LexicalCast<std::set<T>, std::string> {
+public:
+    std::string operator() (const std::set<T>& v) {
+        YAML::Node node;
+        for (auto& i : v) {
+            node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+        }
+        std::stringstream ss;
+        ss << node;
+        return ss.str();
+    }
+};
+
+template<class T>
+class LexicalCast<std::string, std::unordered_set<T> > {
+public:
+    std::unordered_set<T> operator() (const std::string& v) {
+        YAML::Node node = YAML::Load(v);
+        typename std::unordered_set<T> st;
+        std::stringstream ss;
+        for (size_t i = 0; i < node.size(); ++i) {
+            ss.str("");
+            ss << node[i];
+            st.insert(LexicalCast<std::string, T>()(ss.str()));
+        }
+        return st;
+    }
+};
+
+template<class T>
+class LexicalCast<std::unordered_set<T>, std::string> {
+public:
+    std::string operator() (const std::unordered_set<T>& v) {
+        YAML::Node node;
+        for (auto& i : v) {
+            node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+        }
+        std::stringstream ss;
+        ss << node;
+        return ss.str();
+    }
+};
+
+template<class T>
+class LexicalCast<std::string, std::map<std::string, T> > {
+public:
+    std::map<std::string, T> operator() (const std::string& v) {
+        YAML::Node node = YAML::Load(v);
+        typename std::map<std::string, T> mp;
+        std::stringstream ss;
+        for (auto it = node.begin();
+                it != node.end(); ++it) {
+            ss.str("");
+            ss << it->second;
+            mp.insert(std::make_pair(it->first.Scalar(),  LexicalCast<std::string, T>()(ss.str())));
+        }
+        return mp;
+    }
+};
+
+template<class T>
+class LexicalCast<std::map<std::string, T>, std::string> {
+public:
+    std::string operator() (const std::map<std::string, T>& v) {
+        YAML::Node node;
+        for (auto& i : v) {
+            node[i.first] = YAML::Load(LexicalCast<T, std::string>()(i.second));
+        }
+        std::stringstream ss;
+        ss << node;
+        return ss.str();
+    }
+};
+
+template<class T>
+class LexicalCast<std::string, std::unordered_map<std::string, T> > {
+public:
+    std::unordered_map<std::string, T> operator() (const std::string& v) {
+        YAML::Node node = YAML::Load(v);
+        typename std::unordered_map<std::string, T> mp;
+        std::stringstream ss;
+        for (auto it = node.begin();
+                it != node.end(); ++it) {
+            ss.str("");
+            ss << it->second;
+            mp.insert(std::make_pair(it->first.Scalar(),  LexicalCast<std::string, T>()(ss.str())));
+        }
+        return mp;
+    }
+};
+
+template<class T>
+class LexicalCast<std::unordered_map<std::string, T>, std::string> {
+public:
+    std::string operator() (const std::unordered_map<std::string, T>& v) {
+        YAML::Node node;
+        for (auto& i : v) {
+            node[i.first] = YAML::Load(LexicalCast<T, std::string>()(i.second));
         }
         std::stringstream ss;
         ss << node;
@@ -113,9 +272,10 @@ class Config
 public:
     typedef std::map<std::string, ConfigVarbase::ptr> ConfigVarMap;//v放基类，可以动态绑定
 
-    template<class T>
-
     //创建和查找
+    //关键字name，默认值default_value，描述信息description
+    //如果没有查找到就创建并返回该ConfigVar，如果查找到了直接返回结果
+    template<class T> 
     static typename ConfigVar<T>::ptr Lookup(const std::string& name,
             const T& default_value, const std::string& description = "") {
         auto tmp = Lookup<T>(name);
@@ -135,6 +295,8 @@ public:
         return v;
     }
 
+    //根据name查找
+    //return:成功-ConfigVar指针 失败-nullptr
     template<class T>
     static typename ConfigVar<T>::ptr Lookup(const std::string& name) {
         auto it = s_datas.find(name);
--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -11,6 +11,21 @@ dory::ConfigVar<float>::ptr g_float_value_config =
 dory::ConfigVar<std::vector<int>>::ptr g_int_vec_value_config = 
     dory::Config::Lookup("system.int_vec", std::vector<int> {1,2}, "system int vec");
 
+dory::ConfigVar<std::list<int>>::ptr g_int_list_value_config = 
+    dory::Config::Lookup("system.int_list", std::list<int> {1,2}, "system int list");
+
+dory::ConfigVar<std::set<int>>::ptr g_int_set_value_config = 
+    dory::Config::Lookup("system.int_set", std::set<int> {1,2}, "system int set");
+
+dory::ConfigVar<std::unordered_set<int>>::ptr g_int_uset_value_config = 
+    dory::Config::Lookup("system.int_uset", std::unordered_set<int> {1,2}, "system int uset");
+
+dory::ConfigVar<std::map<std::string, int>>::ptr g_str_int_map_value_config = 
+    dory::Config::Lookup("system.str_int_map", std::map<std::string, int> {{"k", 2}}, "system str int map");
+
+dory::ConfigVar<std::unordered_map<std::string, int>>::ptr g_str_int_umap_value_config = 
+    dory::Config::Lookup("system.str_int_umap", std::unordered_map<std::string, int> {{"k", 2}}, "system str int umap");
+
 //测试yaml
 void print_yaml(const YAML::Node& node, int level) {
     if (node.IsScalar()) {
@@ -43,10 +58,31 @@ void test_config() {
     DORY_LOG_INFO(DORY_LOG_ROOT()) << "before: " << g_int_value_config->getValue();
     DORY_LOG_INFO(DORY_LOG_ROOT()) << "before: " << g_float_value_config->toString();
 
-    auto v = g_int_vec_value_config->getValue();
-    for (auto& i : v) {
-        DORY_LOG_INFO(DORY_LOG_ROOT()) << "before int_vec" << i;
+#define XX(g_var, name, prefix) \
+    {\
+        auto& v = g_var->getValue(); \
+        for (auto& i : v) { \
+            DORY_LOG_INFO(DORY_LOG_ROOT()) << #prefix " " #name ": " << i; \
+        } \
+        DORY_LOG_INFO(DORY_LOG_ROOT()) << #prefix " " #name " yaml: " << g_var->toString(); \
     }
+
+#define XX_M(g_var, name, prefix) \
+    {\
+        auto& v = g_var->getValue(); \
+        for (auto& i : v) { \
+            DORY_LOG_INFO(DORY_LOG_ROOT()) << #prefix " " #name ": {" \
+                << i.first << " - " << i.second << "}"; \
+        } \
+        DORY_LOG_INFO(DORY_LOG_ROOT()) << #prefix " " #name " yaml: " << g_var->toString(); \
+    }
+
+    XX(g_int_vec_value_config, int_vec, before);
+    XX(g_int_list_value_config, int_list, before);
+    XX(g_int_set_value_config, int_set, before);
+    XX(g_int_uset_value_config, int_suet, before);
+    XX_M(g_str_int_map_value_config, str_int_map, before);
+    XX_M(g_str_int_umap_value_config, str_int_umap, before);
 
     YAML::Node root = YAML::LoadFile("/home/lly/projectfile/dory/workspace/bin/conf/log.yml");
     dory::Config::LoadFromYaml(root);
@@ -54,10 +90,12 @@ void test_config() {
     DORY_LOG_INFO(DORY_LOG_ROOT()) << "after: " << g_int_value_config->getValue();
     DORY_LOG_INFO(DORY_LOG_ROOT()) << "after: " << g_float_value_config->toString();
 
-    v = g_int_vec_value_config->getValue();
-    for (auto& i : v) {
-        DORY_LOG_INFO(DORY_LOG_ROOT()) << "after int_vec" << i;
-    }
+    XX(g_int_vec_value_config, int_vec, after);
+    XX(g_int_list_value_config, int_list, after);
+    XX(g_int_set_value_config, int_set, after);
+    XX(g_int_uset_value_config, int_uset, after);
+    XX_M(g_str_int_map_value_config, str_int_map, after);
+    XX_M(g_str_int_umap_value_config, str_int_umap, after);
 }
 
 int main(int argc, char** argv) {
